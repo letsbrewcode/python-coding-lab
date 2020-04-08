291231844fbbce3e80c8b52bc201f799638716ed
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-04-07 20:44:53 -0700

    Create detect_duplicate.py

---
+++ lab-4/solutions/detect_duplicate.py
@@ -0,0 +1,36 @@
+# Contains Duplicate
+
+# Given an array of integers, find if the array contains any duplicates.
+# Your function should return true if any value appears at least twice
+# in the array, and it should return false if every element is distinct.
+
+# Example 1:
+# Input: [1,2,3,1]
+# Output: True
+
+# Example 2:
+# Input: [1,2,3,4]
+# Output: False
+
+def contains_duplicate(arr):
+    counts = set()
+    for x in arr:
+        if x in counts:
+            return True
+        else:
+            counts.add(x)
+    return False
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+if __name__ == '__main__':
+    test(contains_duplicate([1,2,3,1]), True)
+    test(contains_duplicate([1,2,3,4]), False)
+    test(contains_duplicate([1,1,1,3,3,4,3,2,4,2]), True)
+
+
