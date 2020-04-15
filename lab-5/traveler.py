803880c39e447a1fd443bba68da882fe788d9228
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-04-14 19:22:45 -0700

    Create traveler.py

---
+++ lab-5/traveler.py
@@ -0,0 +1,32 @@
+# Find end destination of travel route
+# Imagine a 2D coordinate system centered at (0,0), You are given the
+# route of a traveling point in the form of array. Each item of the array
+# contains a direction and distace moved in that direction. Complete the
+# function, destination to compute the route and return the final coordinate
+# where the point finishes its travel. The answer should be returned in
+# the form of a tuple, (x, y)
+
+# Example
+# Input = [['N', 1], ['E', 1]]
+# Output = (1, 1)
+# The point moves 1 unit north and then 1 unit east resulting in the final
+# destination as x = 1 and y = 1. It is returned as tuple, (1, 1)
+
+def destination(route):
+    return
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+if __name__ == '__main__':
+    route1 = [['E', 2], ['N', 5], ['W', 1]]
+    route2 = [['E', 4], ['N', 10], ['W', 7], ['S', 7], ['E', 10]]
+    route3 = [['E', 10], ['N', 10], ['W', 10], ['S', 5], ['S', 5]]
+    
+    test(destination(route1), (1, 5))
+    test(destination(route2), (7, 3))
+    test(destination(route3), (0, 0))
