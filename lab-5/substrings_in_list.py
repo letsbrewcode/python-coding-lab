31842a7227d1263712b8741159c27cc4918b160d
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-04-14 19:22:13 -0700

    Create substrings_in_list.py

---
+++ lab-5/substrings_in_list.py
@@ -0,0 +1,33 @@
+# Given an array of string words. Return all strings in words which is substring of another word in any order. 
+# String words[i] is substring of words[j], if can be obtained removing some characters to left and/or
+# right side of words[j].
+
+# Example 1:
+# Input: words = ["mass","as","hero","superhero"]
+# Output: ["as","hero"]
+# Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+# ["hero","as"] is also a valid answer.
+
+# Example 2:
+# Input: words = ["universe","un","verse"]
+# Output: ["un","verse"]
+# Explanation: "un", "verse" are substring of "universe".
+
+# Example 3:
+# Input: words = ["blue","green","bu"]
+# Output: []
+
+def substrings(words):
+    return
+
+def test(got, expected):
+    if sorted(got) == sorted(expected):
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+if __name__ == '__main__':
+    test(["mass","as","hero","superhero"], ["un","verse"])
+    test(["universe","un","verse"], ["un","verse"])
+    test(["blue","green","bu"], [])
