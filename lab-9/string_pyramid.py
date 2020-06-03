e5b328ce86ebcd060746220558929e307c4cb564
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-06-02 18:54:19 -0700

    Create string_pyramid.py

---
+++ lab-9/string_pyramid.py
@@ -0,0 +1,23 @@
+# Complete the funtion below
+# Given a string, print a pyramid by printing each character of a 
+# string on a new line. If its the nth character then it should be
+# repeated n times.
+# If input string is None or empty then print an '*'
+# Example, for s = 'Saturn'
+# Output:
+# S
+# aa
+# ttt
+# uuuu
+# rrrrr
+# nnnnnn
+
+def print_pyramid(s):
+    # +++ Your code here +++
+    return
+
+if __name__ == '__main__':
+    test_case = ['Saturn', '123456789', '']
+    for t in test_case:
+        print(t)
+        print_pyramid(t)
