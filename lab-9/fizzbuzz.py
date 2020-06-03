94db9798d25cd6d9542da629da226f310b3c235f
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-06-02 18:53:22 -0700

    Create fizzbuzz.py

---
+++ lab-9/fizzbuzz.py
@@ -0,0 +1,36 @@
+"""
+    Problem: FizzBuzz
+
+    Complete the function below. For a number N, print all the numbers starting
+    from 1 to N, except
+    1. For numbers divisible by 3, print "Fizz"
+    2. For numbers divisible by 5, print "Buzz"
+    3. For numbers divisible by both 3 and 5, print "FizzBuzz"
+
+    Example: N = 30
+    Output:
+    1
+    2
+    Fizz
+    4
+    Buzz
+    Fizz
+    7
+    8
+    Fizz
+    Buzz
+    11
+    Fizz
+    13
+    14
+    FizzBuzz
+    16
+"""
+
+def fizzbuzz(n):
+    # +++ Your code here +++
+    return
+
+if __name__ == "__main__":
+    fizzbuzz(100)
+
