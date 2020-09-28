45feed44586e0bf4efca2fcc949410275b1e03c2
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-09-27 20:15:54 -0700

    Create palindrome.py

---
+++ lab-11/palindrome.py
@@ -0,0 +1,15 @@
+# Palindrome
+# Write a method name is_palindrome which accepts a string a input.
+# function wull return True is string is a palindrome else it will
+# return False
+# What test cases will you write to test it?
+
+def is_palindrome(s):
+    return
+
+if __name__ == '__main__':
+    test_case = ['kayak', '12321', 'abcd', 'Malayalam']
+    
+    for t in test_case:
+        print(t, end=" -> ")
+        print(is_palindrome(t))
