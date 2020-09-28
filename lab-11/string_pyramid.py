c8f3ec478b2b40e43ecfab31ae617010eb330729
letsbrewcode <50429097+letsbrewcode@users.noreply.github.com>
2020-09-27 20:14:26 -0700

    Create string_pyramid.py

---
+++ lab-11/string_pyramid.py
@@ -0,0 +1,19 @@
+# Complete the funtion below
+# Given a string, print a pyramid by printing all the substrings from index 0. 
+# If input string is None or empty then print an '*'
+# All the letters should be printed uppercase
+# Example, for s = 'Saturn'
+# Output:
+# S
+# SA
+# SAT
+# SATU
+# SATUR
+# SATURN
+
+def print_pyramid(s):
+    return
+
+if __name__ == '__main__':
+    print_pyramid('')
+    print_pyramid('Saturn')
